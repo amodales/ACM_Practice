a0a008e21bdcb2b078955decb39eb6518fb23c8b
amodales <amodales@gmail.com>
2015-04-14 13:14:19 +0800

    Q119&Q120 Finished. Q121&Q122 Q-Pages Downloaded.

--- Q119/code.c
+++ Q119/code.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct Person{
+	char name[13];
+	int balance;
+}Person;
+
+typedef struct Data{
+	Person people[10];
+	int size;
+}Data;
+
+int Find_Index(Data, char []);
 
 int main(){
+	int n, i, j, amount, div, giver_index, rec_index, c=0;
+	char ch, giver_name[13], rec_name[13];
+	Data database;
+	while(scanf("%d\n", &n)!=EOF){
+		if(c!=0)	printf("\n");
+		memset(database.people, 0, sizeof(database.people));
+		database.size = n;
+		for(i=0; i<n; i++){
+			scanf("%s", database.people[i].name);
+		}
+		for(i=0; i<n; i++){
+			scanf("%s %d %d ", giver_name, &amount, &div);
+			giver_index = Find_Index(database, giver_name);
+			if(div!=0){
+				for(j=0; j<div; j++){
+					scanf("%s", rec_name);
+					rec_index = Find_Index(database, rec_name);
+					database.people[giver_index].balance -= amount/div;
+					database.people[rec_index].balance += amount/div;
+				}
+			}
+		}
+		for(i=0; i<n; i++){
+			printf("%s %d\n", database.people[i].name, database.people[i].balance);
+		}
+		c++;
+	}
 	return 0;
+}
+
+int Find_Index(Data database, char name[]){
+	int i;
+	for(i=0; i<database.size; i++){
+		if(strcmp(database.people[i].name, name)==0){
+			return i;
+		}
+	}
+	return -1;
 }
--- Q120/code.c
+++ Q120/code.c
@@ -1,6 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void Flip(int [], int);
+void Print(int [], int);
 
 int main(){
+	int n = 0, num = 0, i, j, max, index;
+	int stack[30];
+	char ch;
+	memset(stack, 0, sizeof(stack));
+	while(scanf("%c", &ch)!=EOF){
+		switch(ch){
+			case '0': case '1': case '2': case '3':
+			case '4': case '5': case '6': case '7':
+			case '8': case '9':
+				num = num * 10 + ch - '0';
+				break;
+			case ' ':
+				/*
+					put pancake into stack.
+				*/
+				stack[n++] = num;
+				/*
+					Reset parameter.
+				*/
+				num = 0;
+				break;
+			case '\n':
+				/*
+					Put pancake into stack.
+				*/
+				stack[n++] = num;
+				Print(stack, n);
+				/*
+					Start sorting.
+					Initial value of m = n.
+					Step 1: Find the max value of sub-stack(1-m).
+					Step 2: Check if the max value is on the bottom of stack.
+					If it is, then m = m - 1, and go back to step 1.
+					If it isn't, then flip the max value to the top of the stack,
+					flip the max value back to the bottom of the stack,
+					and then m = m - 1, and go back to step 1.
+					Step 3: while m = 0, print out 0.
+				*/
+				for(i=n-1; i>=0; i--){
+					max = 0;
+					index = -1;
+					for(j=0; j<=i; j++){
+						if(stack[j]>max){
+							index = j;
+							max = stack[j];
+						}
+					}
+					if(index!=i){
+						if(index!=0){
+							Flip(stack, index);
+							printf("%d ", n-index);
+						}
+						Flip(stack, i);
+						printf("%d ", n-i);
+					}
+				}
+				printf("0\n");
+				/*
+					Reset parameter.
+				*/
+				num = 0;
+				n = 0;
+				memset(stack, 0, sizeof(stack));
+				break;
+			default:
+				break;
+		}
+	}
 	return 0;
+}
+
+void Flip(int stack[], int bottom){
+	int i, temp, limit;
+	limit = bottom/2 + 1;
+	for(i=0; i<limit; i++){
+		temp = stack[i];
+		stack[i] = stack[bottom-i];
+		stack[bottom-i] = temp;
+	}
+}
+
+void Print(int stack[], int num){
+	int i;
+	for(i=0; i<num; i++){
+		printf("%d", stack[i]);
+		if(i!=num-1)	printf(" ");
+	}
+	printf("\n");
 }
