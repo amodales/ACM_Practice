aa0783c53d1e56972682cfe0f41d1a4fd51e3749
amodales <amodales@gmail.com>
2015-04-22 21:15:04 +0800

    Q110 Finished. Q107 Finished, but its performance still need to be improved.

--- Q107/code.c
+++ Q107/code.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
-	test comment.
-*/
+typedef unsigned long long llu_int;
+
+llu_int Power_N(llu_int, llu_int);
 
 int main(){
-	int h, n;
-	while(scanf("%d %d", &h, &n)==2){
-		if(h==0&&n==0)	break;	
+	llu_int h, n, m, fac, power, found, sum_h, sum_work, num;
+	while(scanf("%llu %llu", &h, &n)==2){
+		if(n>h)	continue;
+		if(h==0&&n==0)	break;
+		if(n!=1||h!=1){
+			found = 0;
+			for(m=1; m<=200&&!found; m++){
+				for(fac=1; fac<=1000; fac++){
+					if(Power_N(fac, m)==n&&Power_N(fac+1, m)==h){
+						found = 1;
+						power = m;
+						break;
+					}
+				}
+			}
+			if(!found&&n==1)	fac=1;
+			num = 1;
+			sum_h = 0;
+			sum_work = 0;
+			while(h!=0){
+				sum_h += h*num;
+				sum_work += num;
+				num *= fac;
+				h/=(fac+1);
+			}
+			printf("%llu %llu\n", sum_work-n, sum_h);
+		}else{
+			printf("0 1\n");
+		}
 	}	
 	return 0;
+}
+
+llu_int Power_N(llu_int base, llu_int n){
+	llu_int m=base, ret=1;
+	while(n!=0){
+		if(n%2==1){
+			ret *= m;
+		}
+		m*=m;
+		n/=2;
+	}
+	return ret;
 }
--- Q110/code.c
+++ Q110/code.c
@@ -1,44 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void print_line(int *, int);
+void Print_If_ElseIf_Else(int, int, int []);
+void Print_Line(int);
 
 int main(){
-	int n, i, *line;
-	scanf("%d", &n);
-	line = (int*) malloc( sizeof(int)*n );
-	for(i=0; i<n; i++){
-		line[i] = i;
+	int m, n, i, count=0, line[8];
+	scanf("%d", &m);
+	while(count<m){
+		if(count!=0)	printf("\n");
+		scanf("%d", &n);
+		for(i=0; i<n; i++){
+			line[i] = i;
+		}
+		/*
+			HEAD
+		*/
+		printf("program sort(input,output);\n");
+		printf("var\n");
+		Print_Line(n);
+		printf(" : integer;\n");
+		printf("begin\n");
+		printf("  readln(");
+		Print_Line(n);
+		printf(");\n");
+		/*
+			BODY
+		*/
+		if(n>1){
+			line[0] = 0;
+			Print_If_ElseIf_Else(2, n, line);
+		}else{
+			printf("  writeln(a)\n");
+		}
+		/*
+			FOOT
+		*/
+		printf("end.\n");
+		count++;
 	}
-	/*
-		HEAD
-	*/
-	printf("program sort(input,output);\n");
-	printf("var\n");
-	print_line(line, n);
-	printf(" : integer;\n");
-	printf("begin\n");
-	printf("\treadln(");
-	print_line(line, n);
-	printf(");\n");
-	/*
-		BODY
-	*/
-	/*
-		FOOT
-	*/
-	printf("end.\n");
-	/*
-		Release memory
-	*/
-	free(line);
 	return 0;
 }
 
-void print_line(int *line, int n){
+void Print_If_ElseIf_Else(int num, int max, int perm[]){
+	int i, j, temp, line[8];
+	/*
+		Make a copy of perm.
+	*/
+	for(i=0; i<num-1; i++){
+		line[i] = perm[i];
+	}
+	line[num-1] = num-1;
+	for(i=num-1; i>=0; i--){
+		for(j=0; j<(num-1)*2; j++)	printf(" ");
+		if(i==num-1){
+			printf("if %c < %c then\n", line[i-1]+'a', num+'a'-1);
+		}else if(i>0&&i<num-1){
+			printf("else if %c < %c then\n", line[i-1]+'a', num+'a'-1);
+		}else{
+			printf("else\n");
+		}
+		if(num<max){
+			Print_If_ElseIf_Else(num+1, max, line);
+		}else{
+			for(j=0; j<num*2; j++)	printf(" ");
+			printf("writeln(");
+			for(j=0; j<max; j++){
+				printf("%c", line[j]+'a');
+				if(j!=max-1)	printf(",");
+			}
+			printf(")\n");
+		}
+		if(i!=0){
+			temp = line[i];
+			line[i] = line[i-1];
+			line[i-1] = temp;
+		}
+	}
+}
+
+void Print_Line(int n){
 	int i;
 	for(i=0; i<n; i++){
-		printf("%c", 'a'+i);
-		if(i+1!=n)	printf(",");
+		printf("%c", i+'a');
+		if(i!=n-1)	printf(",");
 	}
 }
